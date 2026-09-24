202e0172afa450a1aa7af05f2220a4e4b7c000cd
Hernando Cadet <58520366+hcadet@users.noreply.github.com>
2026-09-24 16:35:58 -0400

    Tableau Web Portal Fixes13

--- components/TableauDashboard.tsx
+++ components/TableauDashboard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+//import React, { useEffect, useState } from 'react';
 import * as React from 'react';
 
 // Declare custom HTML element for TypeScript
